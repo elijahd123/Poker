43e0e6ce531e947ea8caf12dc1250746b3293faf
Elijah Deal <elijahrdeal@gmail.com>
2022-06-20 21:00:20 +0100

    Fourth Commit

--- Cards/card.py
+++ Cards/card.py
@@ -3,7 +3,7 @@ from suits import Suits
 card_values_to_names = {1: "Ace", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight",
                         9: "Nine", 10: "Ten", 11: "Jack", 12: "Queen", 13: "King"}
 
-
+# ianheguhaougnaiengaipeghiaghiaegnpi
 class Card:
     def __init__(self, value: int, suit: Suits):
         if value not in card_values_to_names:
